1004c514d0289530f97cde0369c3d8b3b912e0a5
Huynh Thi Khanh Linh <bongudth@users.noreply.github.com>
2021-08-15 17:54:42 +0700

    :hammer: update background of ContactUs

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <div id="app">
+    <ContactUs />
   </div>
 </template>
 
 <script>
+import ContactUs from "./components/ContactUs.vue";
+
 export default {
   name: "App",
   components: {
+    ContactUs,
   },
 };
 </script>
 
 <style lang="scss">
 #app {
-  font-family: 'Open Sans', sans-serif;
+  font-family: "Open Sans", sans-serif;
   // font-family: 'Montserrat', sans-serif;
   // font-family: 'Oswald', sans-serif;
   // font-family: 'Roboto', sans-serif;
---
+++ src/components/ContactUs.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="contact-us">
+    <div class="background"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ContactUs",
+};
+</script>
+
+<style lang="scss" scoped>
+#contact-us {
+  .background {
+    background-image: url("../assets/dsff-min.jpg");
+    height: 697px;
+  }
+}
+</style>
